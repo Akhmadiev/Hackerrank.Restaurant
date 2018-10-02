5275b7929d73f1aa73f465c74b22016686d8d487
Marat Akhmadiev <akhmadiev1993@outlook.com>
2018-10-02 16:15:40 +0300

    Add project files.

---
+++ Hackerrank.Restaurant/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hackerrank.Restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(restaurant(391, 567));
+        }
+
+        static int restaurant(int l, int b)
+        {
+            var square = l * b;
+
+            for (var i = l; i > 0; i--)
+            {
+                if (l % i == 0)
+                {
+                    if (b % i == 0)
+                    {
+                        return square / (i * i);
+                    }
+                }
+            }
+
+            return square;
+        }
+    }
+}
